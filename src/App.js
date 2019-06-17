577b78fd188e7c7e84d03fcbd540310828e05302
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-06-17 11:51:22 +0100

    todo completed state toggle done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       {
         id: 2,
         title: 'Dinner with Green',
-        completed: false
+        completed: true
       },
       {
         id: 3,
@@ -23,10 +23,22 @@ class App extends Component {
       }
     ]
   }
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
   render() {
       return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
-    getStyle = () => {}
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            marginBottom: '1px',
+            textDecoration: 
+            this.props.todo.completed ? 
+            'line-through' : 'none'
+        }
+    }
+
     render() { 
+        const { title, id } = this.props.todo;
         return ( 
         <div style={this.getStyle()} >
-          <h3>{ this.props.todo.title }</h3>  
+          <h3>
+              <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+          { title }
+          </h3>  
         </div> 
         );
     }
