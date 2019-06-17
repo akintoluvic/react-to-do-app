e27a40622206da899d68fb6d9e4d009610cd054f
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-06-17 10:33:14 +0100

    created Todos component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with Green',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting Jamon',
+        completed: false
+      }
+    ]
+  }
+  render() {
+      return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Todos</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Todos;
